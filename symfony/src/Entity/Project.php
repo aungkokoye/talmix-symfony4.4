24536a14ea9ca8356682bfef235f3b5942d01dfc
Aung Ko Ko Ye <aungkokoye@gmail.com>
2024-02-15 11:19:40 +0000

    add serialization

--- symfony/src/Entity/Project.php
+++ symfony/src/Entity/Project.php
@@ -5,14 +5,32 @@ namespace App\Entity;
 use App\Repository\ProjectRepository;
 use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
+Use Symfony\Component\Serializer\Annotation\Groups;
+Use Symfony\Component\Serializer\Annotation\SerializedName;
 
 /**
- * @ApiResource
+ * @ApiResource(
+ *     collectionOperations={"get", "post"},
+ *     itemOperations={
+ *     "get" = {"path"="/my-project/{id}"},
+ *      "put"
+ *     },
+ *     normalizationContext={
+ *      "groups"={"project:read"}
+ *     },
+ *     denormalizationContext={
+ *       "groups"={"project:write"}
+ *     }
+ *
+ * )
  * @ORM\Entity(repositoryClass=ProjectRepository::class)
  */
 class Project
 {
     /**
+     *
+     * @Groups({"project:read"})
+     *
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
@@ -20,14 +38,39 @@ class Project
     private $id;
 
     /**
+     * @Groups({"project:read", "project:write"})
+     *
      * @ORM\Column(type="string", length=255)
      */
     private $name;
 
     /**
+     * The value of project in pences.
+     *
+     *  @Groups({"project:read", "project:write"})
+     *
      * @ORM\Column(type="integer")
      */
     private $value;
+
+    /**
+     *  @Groups({"project:read", "project:write"})
+     *
+     * @ORM\ManyToOne(targetEntity=ProjectOwner::class, inversedBy="projects")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $owner;
+
+    /**
+     * @Groups({"project:read",  "project:write"})
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    public function __construct(string $description = null)
+    {
+        $this->description = $description;
+    }
 
     public function getId(): ?int
     {
@@ -56,5 +99,44 @@ class Project
         $this->value = $value;
 
         return $this;
+    }
+
+    public function getOwner(): ?ProjectOwner
+    {
+        return $this->owner;
+    }
+
+    public function setOwner(?ProjectOwner $owner): self
+    {
+        $this->owner = $owner;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+//    prevent overwrite in edit action, property is set in constructor method for create action
+//    public function setDescription(?string $description): self
+//    {
+//        $this->description = $description;
+//
+//        return $this;
+//    }
+
+    /**
+     * @SerializedName("decription_upper")
+     * @Groups({"project:read"})
+     *
+     * @return ?string
+     */
+    public function getDescriptionCapital(): ?string
+    {
+        return strtoupper($this->description);
     }
 }
---
+++ symfony/src/Entity/ProjectOwner.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\ProjectOwnerRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=ProjectOwnerRepository::class)
+ */
+class ProjectOwner
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Project::class, mappedBy="owner", cascade={"remove"})
+     */
+    private $projects;
+
+    public function __construct()
+    {
+        $this->projects = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Project>
+     */
+    public function getProjects(): Collection
+    {
+        return $this->projects;
+    }
+
+    public function addProject(Project $project): self
+    {
+        if (!$this->projects->contains($project)) {
+            $this->projects[] = $project;
+            $project->setOwner($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProject(Project $project): self
+    {
+        if ($this->projects->removeElement($project)) {
+            // set the owning side to null (unless already changed)
+            if ($project->getOwner() === $this) {
+                $project->setOwner(null);
+            }
+        }
+
+        return $this;
+    }
+}
